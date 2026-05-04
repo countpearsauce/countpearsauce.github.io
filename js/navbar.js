6972123928af89eb9e5afc2383232ba4e14a9bc6
Jeremy Lei <jeremy.lei.2002@gmail.com>
2026-05-03 21:16:54 -0700

    navbar for index page

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+// Function to load the navbar
+function loadNavbar() {
+    fetch('components/navbar.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('navbar-container').innerHTML = data;
+      })
+      .catch(error => console.error('Error loading the navbar:', error));
+  }
+  
+  // Run the function when the page loads
+  window.onload = loadNavbar;
